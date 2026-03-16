4d1d8c56cda7773f84477b8a093932a7c011f49f
vibhav chirravuri <44679811+VibhavChirravuri2001@users.noreply.github.com>
2026-03-16 03:20:42 -0700

    Optimize I/O performance in Distinct Numbers
    Added synchronization for faster I/O operations.

--- Sorting And Searching/Distinct Numbers.cpp
+++ Sorting And Searching/Distinct Numbers.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -17,6 +25,9 @@ int DistinctNumbers(int n, vector<int>a){
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int n;
     cin>>n;
     
