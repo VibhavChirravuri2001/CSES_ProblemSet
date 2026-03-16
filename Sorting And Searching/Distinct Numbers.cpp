6199f1b0249ff0e5fdf4de70bd027593d8e5f973
vibhav chirravuri <44679811+VibhavChirravuri2001@users.noreply.github.com>
2026-03-16 01:54:14 -0700

    Implement function to count distinct numbers

---
+++ Sorting And Searching/Distinct Numbers.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int DistinctNumbers(int n, vector<int>a){
+    unordered_map<int,int>m;
+    
+    for(auto i=0;i<n;i++){
+        m[a[i]]++;
+    }
+    
+    
+    return m.size();
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    vector<int>a;
+    
+    for(auto i=0;i<n;i++){
+        int ins;
+        cin>>ins;
+        
+        a.push_back(ins);
+    }
+    
+    cout<<DistinctNumbers(n,a)<<endl;
+
+    return 0;
+}
